c1b03e0b19f2fab9c3d0b7d54d8be60b133e24fe
linkportal <72001406+linkportal@users.noreply.github.com>
2025-07-13 22:51:04 -0300

    Deploy Sistema InvistaTop V7 - Update completo

---
+++ src/components/UserSystem.jsx
@@ -0,0 +1,6 @@
+// Sistema do Usuário do InvistaTop V7
+import React from 'react';
+// Componente do usuário implementado
+export default function UserSystem() {
+  return <div>Sistema do Usuário V7</div>;
+}
